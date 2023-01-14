805b0e5667a2ddd8c1761f8b1c69655e0040fb6d
jackhnguyen <jack.h.n.97@gmail.com>
2023-01-14 15:55:46 -0800

    got the db and foward auto

---
+++ Elevator/src/main/java/frc/robot/commands/Autos.java
@@ -0,0 +1,26 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.subsystems.Drivebase;
+import frc.robot.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+
+public final class Autos {
+  /** Example static factory for an autonomous command. */
+  public static CommandBase FowardFor3Secs(Drivebase m_db) {
+    return Commands.sequence(
+      m_db.forward(1.0),
+      new WaitCommand(3.0), 
+      m_db.forward(0.0)
+      );
+  }
+
+  private Autos() {
+    throw new UnsupportedOperationException("This is a utility class!");
+  }
+}
